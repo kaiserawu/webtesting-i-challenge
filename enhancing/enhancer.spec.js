8abc718fcf014ac5ba2c783d7550fec23e4020e5
kaiserawu <kaiser.a.wu@gmail.com>
2019-06-19 20:33:16 -0700

    Implement repair function, write basic tests for succeed enhancement

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -2,7 +2,13 @@ const enhancer = require('./enhancer.js');
 // test away!
 describe('The enhancer functions', () => {
   const item = { name: 'The Sword', durability: 40, enhancement: 0 };
+
   const repaired = enhancer.repair(item);
+
+  const succeeded = enhancer.succeed(item);
+  const maxed = { ...item, enhancement: 20 };
+  const succeededMaxed = enhancer.succeed(maxed);
+
   
   it('should repair an item to 100 durability', () => {
     expect(repaired.durability).toEqual(100);
@@ -15,6 +21,25 @@ describe('The enhancer functions', () => {
     expect(Object.keys(repaired)).toContain('durability');
     expect(Object.keys(repaired)).toContain('enhancement');
     expect(repaired.enhancement).toEqual(item.enhancement);
+  })
+
+  it('should increase enhancement level if applicable', () => {
+    expect(succeeded.enhancement).toEqual(item.enhancement + 1);
+    expect(succeededMaxed.enhancement).toEqual(20);
+  })
+  it('should not do anything else wonky when successfully enhanced', () => {
+    expect(typeof succeeded).toBe('object');
+    expect(Object.keys(succeeded)).toContain('name');
+    expect(succeeded.name).toEqual(item.name);
+    expect(Object.keys(succeeded)).toContain('durability');
+    expect(succeeded.durability).toEqual(item.durability);
+    expect(Object.keys(succeeded)).toContain('enhancement');
+    expect(typeof succeededMaxed).toBe('object');
+    expect(Object.keys(succeededMaxed)).toContain('name');
+    expect(succeededMaxed.name).toEqual(item.name);
+    expect(Object.keys(succeededMaxed)).toContain('durability');
+    expect(succeededMaxed.durability).toEqual(item.durability);
+    expect(Object.keys(succeededMaxed)).toContain('enhancement');
   })
   
 })
